464d2d8f771575248886133e1daaeb73c4d974b5
Happy <insisthzr@163.com>
2022-07-02 20:00:52 +0800

    delete comments

--- src/App.js
+++ src/App.js
@@ -40,39 +40,4 @@ const App = () => {
   );
 }
 
-// class App extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       monsters: [],
-//       searchField: "",
-//     };
-//   }
-//   async componentDidMount() {
-//     const response = await fetch("https://jsonplaceholder.typicode.com/users");
-//     const users = await response.json()
-//     this.setState({ monsters: users });
-//   }
-//   onSearchChange = (e) => {
-//     const searchField = e.target.value.toLocaleLowerCase();
-//     this.setState(() => {
-//       return { searchField };
-//     });
-//   }
-//   render() {
-//     const { monsters, searchField } = this.state;
-//     const { onSearchChange } = this;
-//     const filteredMonsters = monsters.filter(monster => {
-//       return monster.name.toLocaleLowerCase().includes(searchField);
-//     });
-//     return (
-//       <div className="App">
-//         <h1 className="app-title">Monsters Rolodex</h1>
-//         <SearchBox onChangeHandler={onSearchChange} placeholder="search monsters" className="monsters-search-box" />
-//         <CardList monsters={filteredMonsters} />
-//       </div >
-//     );
-//   }
-// }
-
 export default App;
